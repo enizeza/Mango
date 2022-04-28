d612f0bd62f35ed7f09d487b9ca361d8c52d9afb
cracolicil <leonardo.cracolici@gmail.com>
2022-04-28 18:33:09 +0200

    Interfaccia grafica product screen

--- frontend/src/ProductScreen.js
+++ frontend/src/ProductScreen.js
@@ -1,17 +1,49 @@
+import { Button } from '@mui/material';
+import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import data from './data';
+import { Store } from './Store';
+import './ProductScreen.css';
 
 function ProductScreen() {
   const params = useParams();
   const { slug } = params;
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
 
   let found = data.products.find((product) => product.slug === slug);
 
+  const addToCart = () => {
+    const existItem = cart.cartItems.find((x) => x._id === found._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    dispatch({
+      type: 'CART_ADD_ITEM',
+      payload: {
+        ...found,
+        quantity,
+      },
+    });
+  };
   return (
-    // Da implementare interfaccia grafica del prodotto
-    <div>
-      <h1>{found.name}</h1>
-      <h1>{found.price}</h1>
+    <div className="product-screen">
+      <div className="product-image">
+        <img src={found.image} className="product-image" alt={found.name}></img>
+      </div>
+      <div className="product-detail">
+        <h1 className="product-title">{found.name}</h1>
+        <p>€{found.price}</p>
+        <p>Description:</p>
+        <p className="product-description">{found.description}</p>
+        {found.countInStock === 0 ? (
+          <Button variant="contained" disabled>
+            Out of stock
+          </Button>
+        ) : (
+          <Button onClick={addToCart} variant="contained">
+            Add to cart
+          </Button>
+        )}
+      </div>
     </div>
   );
 }

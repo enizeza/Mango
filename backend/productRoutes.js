a61c775bfd69482af904ca30cfb90715881b7efe
enizeza <enizeza@live.it>
2022-06-03 11:52:54 +0200

    filtri categorie

--- backend/productRoutes.js
+++ backend/productRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import expressAsyncHandler from 'express-async-handler';
+
 import Product from './productModel.js';
 
 const productRouter = express.Router();
--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
@@ -6,6 +6,9 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 import { Link } from 'react-router-dom';
 import { Store } from './Store';
+import axios from 'axios';
+import { getError } from './utils';
+import { toast } from 'react-toastify';
 
 function Header() {
   const { state, dispatch } = useContext(Store);
@@ -17,6 +20,21 @@ function Header() {
     localStorage.removeItem('shippingInfo');
     window.location.href = '/signin';
   };
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const { data } = await axios.get(`/api/products/categories`);
+        setCategories(data);
+      } catch (err) {
+        toast.error(getError(err));
+      }
+    };
+    fetchCategories();
+  }, []);
+
   return (
     <nav className="navbar">
       <Link to="/">
@@ -27,11 +45,16 @@ function Header() {
       <div className="item search right" tabIndex="0">
         <div className="search-group">
           <select>
-            <option value="all">All</option>
+            {categories.map((category) => (
+              <option value={category}>
+                <Link to={`/search?category=${category}`}>{category}</Link>
+              </option>
+            ))}
+            {/*<option value="all">All</option>
             <option value="all">Mens</option>
             <option value="all">Womens</option>
             <option value="all">Winter</option>
-            <option value="all">Summer</option>
+            <option value="all">Summer</option>*/}
           </select>
           <input type="text" />
           <i className="material-icons search-icon">

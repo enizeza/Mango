b95d73a1b8aba59aaa275d2badbde2c8c1088dc0
enizeza <enizeza@live.it>
2022-06-16 14:22:26 +0200

    Risolto piccolo bug fetch product da backend

--- frontend/src/ProductScreen.js
+++ frontend/src/ProductScreen.js
@@ -1,22 +1,52 @@
 import { Button } from '@mui/material';
-import { useContext } from 'react';
+import { useContext, useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
-import data from './data';
+import axios from 'axios';
 import { Store } from './Store';
 import './ProductScreen.css';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 function ProductScreen() {
   const params = useParams();
   const { slug } = params;
-  const { state, dispatch } = useContext(Store);
+  const { state, dispatch: ctxDispatch } = useContext(Store);
   const { cart } = state;
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    products: [],
+    loading: true,
+    error: '',
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+    };
+    fetchData();
+  }, []);
 
-  let found = data.products.find((product) => product.slug === slug);
+  let found = products.find((product) => product.slug === slug);
 
   const addToCart = () => {
     const existItem = cart.cartItems.find((x) => x._id === found._id);
     const quantity = existItem ? existItem.quantity + 1 : 1;
-    dispatch({
+    ctxDispatch({
       type: 'CART_ADD_ITEM',
       payload: {
         ...found,
@@ -26,24 +56,32 @@ function ProductScreen() {
   };
   return (
     <div className="product-screen">
-      <div className="product-image">
-        <img src={found.image} alt={found.name}></img>
-      </div>
-      <div className="product-detail">
-        <h1 className="product-title">{found.name}</h1>
-        <p>€{found.price}</p>
-        <p>Description:</p>
-        <p className="product-description">{found.description}</p>
-        {found.countInStock === 0 ? (
-          <Button variant="contained" disabled>
-            Out of stock
-          </Button>
-        ) : (
-          <Button onClick={addToCart} variant="contained">
-            Add to cart
-          </Button>
-        )}
-      </div>
+      {loading ? (
+        <div>Caricamento</div>
+      ) : error ? (
+        <div variant="danger">{error}</div>
+      ) : (
+        <>
+          <div className="product-image">
+            <img src={found.image} alt={found.name}></img>
+          </div>
+          <div className="product-detail">
+            <h1 className="product-title">{found.name}</h1>
+            <p>€{found.price}</p>
+            <p>Description:</p>
+            <p className="product-description">{found.description}</p>
+            {found.countInStock === 0 ? (
+              <Button variant="contained" disabled>
+                Out of stock
+              </Button>
+            ) : (
+              <Button onClick={addToCart} variant="contained">
+                Add to cart
+              </Button>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 }
